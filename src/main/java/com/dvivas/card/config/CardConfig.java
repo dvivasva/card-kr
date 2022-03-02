5cefe9b811dc603a6eceae35476596a4523925fd
David Hilder Vivas Vasquez <dv2_6@hotmail.com>
2022-03-02 17:45:17 -0500

    runnig

--- src/main/java/com/dvivas/card/config/CardConfig.java
+++ src/main/java/com/dvivas/card/config/CardConfig.java
@@ -1,13 +1,39 @@
 package com.dvivas.card.config;
 
+import com.dvivas.card.model.Card;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@EnableRedisRepositories
 @Configuration
 public class CardConfig {
     @Bean
-    BCryptPasswordEncoder bCryptPasswordEncoder(){
-        return  new BCryptPasswordEncoder();
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfig = new RedisStandaloneConfiguration();
+        redisStandaloneConfig.setHostName("localhost");
+        redisStandaloneConfig.setPort(6379);
+        //redisStandaloneConfig.setPassword(redisPassword);
+        return new LettuceConnectionFactory(redisStandaloneConfig);
+    }
+
+    @Bean
+    public ReactiveRedisOperations<String, Card> redisOperations(LettuceConnectionFactory connectionFactory) {
+        RedisSerializationContext<String, Card> serializationContext = RedisSerializationContext
+                .<String, Card>newSerializationContext(new StringRedisSerializer())
+                .key(new StringRedisSerializer())
+                .value(new GenericToStringSerializer<>(Card.class))
+                .hashKey(new StringRedisSerializer())
+                .hashValue(new GenericJackson2JsonRedisSerializer())
+                .build();
+        return new ReactiveRedisTemplate<>(connectionFactory, serializationContext);
     }
 }
--- src/main/java/com/dvivas/card/controller/CardController.java
+++ src/main/java/com/dvivas/card/controller/CardController.java
@@ -4,10 +4,8 @@ import com.dvivas.card.dto.CardDto;
 import com.dvivas.card.service.CardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -17,6 +15,13 @@ public class CardController {
 
 
     private  final CardService cardService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<CardDto> create(
+            @RequestBody final Mono<CardDto> cardDtoMono) {
+        return cardService.create(cardDtoMono);
+    }
 
     @GetMapping("/{numberCard}")
     @Cacheable(value = "Card", key = "numberCard")
